0910e52713de30953ce2c124d8f2692a46a8af09
Mohammad Farzan <m2_farzan@yahoo.com>
2021-02-26 17:42:20 +0330

    Use VirtualBox IP's

--- core.py
+++ core.py
@@ -4,7 +4,7 @@ import re
 def get_my_ip():
     response = subprocess.Popen(['ip', 'addr', 'show'],
         stdout=subprocess.PIPE).stdout.read().decode('utf-8')
-    ips = re.findall(r'172\.\d+\.\d+\.\d+', response)
+    ips = re.findall(r'10\.\d+\.\d+\.\d+', response)
     return ips[0]
 
 def update_emulation_engine(params):
@@ -33,7 +33,7 @@ def update_emulation_engine(params):
 
 def init_iptables():
     my_ip = get_my_ip()
-    host_ip = re.sub(r'\d+$', '1', my_ip)
+    host_ip = re.sub(r'\d+$', '2', my_ip)
     subprocess.run(
         f'iptables -t nat -A PREROUTING -p tcp --dport 90 -j RETURN',
         shell=True
